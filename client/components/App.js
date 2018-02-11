155d2f4087618d230ab3e9cc51671a6e18f3b630
pavankumardhulipudi <pavankumardhulipudi@Pavans-MacBook-Pro.local>
2018-02-11 00:38:11 -0500

    Adding Easy, Medium, Hard logic using min max algorithm

--- client/components/App.js
+++ client/components/App.js
@@ -11,14 +11,13 @@ export default class App extends React.Component {
       { value: "impossible", text: "Impossible" },
       { value: "players", text: "Play against a friend" }
     ];
-
     this.cross = "x";
     this.circle = "circle outline";
-    this.counter = 0;
+    this.currentPlayer = "x";
+    this.isCellClicked = false;
 
     this.state = {
-      level: "players",
-      player: "x",
+      level: "impossible",
       grid: new Array(3).fill('').map(()=>new Array(3).fill('')),
       winner: ""
     };
@@ -27,12 +26,25 @@ export default class App extends React.Component {
     this.resetGrid = this.resetGrid.bind(this);
     this.buildGrid = this.buildGrid.bind(this);
     this.cellClicked = this.cellClicked.bind(this);
+    this.isGameOver = this.isGameOver.bind(this);
+    this.getNextCell = this.getNextCell.bind(this);
+  }
 
+  componentDidUpdate(){
+    let { level, grid } = this.state;
+    let currentPlayer = this.currentPlayer;
+    if(level != "players" && this.isCellClicked) {
+      setTimeout(() => {
+        let cell = this.getNextCell(grid, 0, currentPlayer);
+        this.cellClicked(cell.r, cell.c, false);
+      });
+    }
   }
 
   resetGrid() {
     let grid = new Array(3).fill('').map(()=>new Array(3).fill(''));
-    this.counter = 0;
+    this.currentPlayer = this.cross;
+    this.isCellClicked = false;
     this.setState({grid, winner: ""});
   }
 
@@ -41,26 +53,91 @@ export default class App extends React.Component {
     this.setState({level});
   }
 
-  cellClicked(row, col) {
-    let { grid, player, winner } = this.state;
-    if(grid[row][col] != "" || winner != "") {
+  cellClicked(row, col, isCellClicked) {
+    let { grid, winner } = this.state;
+    if(winner != "" || grid[row][col] != "") {
       return false;
     }
-    this.counter ++;
-    grid[row][col] = player;
-    winner = this.validateGame(grid);
+    grid[row][col] = this.currentPlayer;
+    winner = this.isGameOver(grid);
     if(winner) {
       this.setState({winner});
+      this.currentPlayer = this.cross;
+      return false;
     } else {
-      player = (player == this.cross) ? this.circle : this.cross;
-      this.setState({grid, player});
+      this.currentPlayer = (this.currentPlayer == this.cross) ? this.circle : this.cross;
+      this.isCellClicked = isCellClicked;
+      this.setState({grid});
     }
+
   }
 
-  validateGame(grid) {
-    if(this.counter < 3) {
-      return false;
+  getNextCell(gridArr, depth, player) {
+    let gridStatus = this.isGameOver(gridArr);
+    if(gridStatus == false) {
+
+      let values = [];
+
+      for(let r=0; r<3; r++) {
+        for(let c=0; c<3; c++) {
+          let grid = JSON.parse(JSON.stringify(gridArr));
+          if(grid[r][c] !== '') continue;
+          grid[r][c] = player;
+          let swapedPlayer = (player == this.cross) ? this.circle : this.cross;
+          let value = this.getNextCell(grid, depth + 1, swapedPlayer);
+          values.push({
+            value,
+            cell: { r, c }
+          });
+        }
+      }
+      let {level} = this.state;
+      if(player == this.circle) {
+        let max;
+        values = values.sort((a,b)=>a.value<b.value);
+        if(level == "impossible") {
+          max = values[0];
+        } else if (level == "medium") {
+          let mid = Math.floor(values.length/2);
+          max = values[mid];
+        } else {
+          max = values[values.length-1];
+        }
+        if(depth == 0) {
+          return max.cell;
+        } else {
+          return max.value;
+        }
+      } else {
+        let min;
+        values = values.sort((a,b)=>a.value>b.value);
+        if(level == "impossible") {
+          min = values[0];
+        } else if (level == "medium") {
+          let mid = Math.floor(values.length/2);
+          min = values[mid];
+        } else {
+          min = values[values.length-1];
+        }
+        if(depth == 0) {
+          return min.cell;
+        } else {
+          return min.value;
+        }
+      }
     }
+    if(gridStatus == "draw") {
+      return 0;
+    } else if(gridStatus == this.cross) {
+      return (depth - 10);
+    } else if(gridStatus == this.circle) {
+      return (10 - depth);
+    }
+
+  }
+
+  isGameOver(grid) {
+    //Horizontal & Vertical
     for(let l=0; l<3; l++) {
       if(grid[l][0] == grid[l][1] && grid[l][1] == grid[l][2] && grid[l][2] != '') {
         return grid[l][0];
@@ -69,15 +146,22 @@ export default class App extends React.Component {
         return grid[0][l];
       }
     }
+    //Diagonal
     if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[2][2] != '') {
       return grid[0][0];
     } else if(grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[2][0] != '') {
       return grid[0][2];
-    } else if(this.counter == 9) {
-      return "draw";
-    } else {
-      return false;
     }
+
+    for(let r=0; r<3; r++) {
+      for(let c=0; c<3; c++) {
+        if(grid[r][c] == '') {
+          return false
+        }
+      }
+    }
+
+    return "draw";
   }
 
   buildGrid() {
@@ -89,7 +173,7 @@ export default class App extends React.Component {
       let gridCols = [];
       for(let c=0; c<cols; c++) {
         gridCols.push(
-          <Grid.Column className="cursor-pointer" key={'col'+r+c} onClick={() => this.cellClicked(r, c)}>
+          <Grid.Column className="cursor-pointer" key={'col'+r+c} onClick={() => this.cellClicked(r, c, true)}>
             <Icon name={grid[r][c]} size="huge"/>
           </Grid.Column>
         );
@@ -102,7 +186,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    let { player, winner } = this.state;
+    let { winner } = this.state;
     let gridContent, gridContentWinner;
 
     if(winner == "draw") {
@@ -122,6 +206,10 @@ export default class App extends React.Component {
           <h3>Winner!</h3>
         </div>
       );
+    } else if (this.isCellClicked) {
+      gridContentWinner = (
+        <div className="app-grid_result"/>
+      );
     }
 
     gridContent = this.buildGrid();
@@ -134,7 +222,7 @@ export default class App extends React.Component {
             <div>
               <Dropdown size="mini" onChange={(e, {value}) => this.changeLevel(value)} options={this.options} value={this.state.level}/>
             </div>
-            <div className="text-right"><Icon name={player}/> Turn</div>
+            <div className="text-right"><Icon name={this.currentPlayer}/> Turn</div>
           </div>
           <div className="app-grid">
             {gridContentWinner}
